aff9e488f2766e1f34c4de529c6a496ab6efc33d
james <james@uswilkis.com>
2026-04-18 18:46:43 -0400

    New changes to Definitions and some new things to ignore

--- Definitions.py
+++ Definitions.py
@@ -1,17 +1,17 @@
-from enum import Enum
+from enum import Enum, auto
 
 class States(Enum):
-    STBY                    # Waiting for ESPs to Connect
-    STBY_READY              # ESPs are connected, waiting for drone
-    STBY_DRONE_LAND         # All devices are connected, waiting for confirmation of drone landing
-    POS_PINCH               # Position Pinch Process Initiated
-    POS_PUSH                # Position Push Process Initiated
-    SWAP_ALIGN              # Swap Alignment Process Initiated
-    SWAP_REMOVE             # Swap Removal Process Initiated
-    SWAP_INSERT             # Swap Inert Process Initiated
-    SWAP_CONNECT            # Attempt to Reconnect to Drone
-    FINAL_POS_PUSH          # Position Push To Final Position Process Initiated
-    FINAL_POS_REL           # Release Drone for Flight
+    STBY               = auto()   # Waiting for ESPs to Connect
+    STBY_READY         = auto()   # ESPs are connected, waiting for drone
+    STBY_DRONE_LAND    = auto()   # All devices are connected, waiting for confirmation of drone landing
+    POS_PINCH          = auto()   # Position Pinch Process Initiated
+    POS_PUSH           = auto()   # Position Push Process Initiated
+    SWAP_ALIGN         = auto()   # Swap Alignment Process Initiated
+    SWAP_REMOVE        = auto()   # Swap Removal Process Initiated
+    SWAP_INSERT        = auto()   # Swap Inert Process Initiated
+    SWAP_CONNECT       = auto()   # Attempt to Reconnect to Drone
+    FINAL_POS_PUSH     = auto()   # Position Push To Final Position Process Initiated
+    FINAL_POS_REL      = auto()   # Release Drone for Flight
     
 
 ESP_SWAP_CONNECTED = False
